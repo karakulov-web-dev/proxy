2d7b24ae0532f8fae7117d4ea9afea97f437a4a3
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-07-23 09:26:44 +0700

    add allowedProxy protection

--- index.js
+++ index.js
@@ -7,44 +7,67 @@ app.use(express.json());
 
 const host = "http://212.77.128.203/nodejsapp/proxy";
 
-function ProxyTableFacade() {
-  this.proxyTable = {};
-  this.proxyTableDateAdded = {};
-  var self = this;
-  setInterval(function() {
-    self.deleteOldProxyUrl();
-  }, 7200000);
-}
-ProxyTableFacade.prototype.add = function(url) {
-  var proxyUrl = "/" + md5(url);
-  this.proxyTable[proxyUrl] = url;
-  this.proxyTableDateAdded[proxyUrl] = Date.now();
-  return host + proxyUrl;
-};
-ProxyTableFacade.prototype.deleteOldProxyUrl = function() {
-  var time = Date.now();
-  for (var key in this.proxyTableDateAdded) {
-    if (this.proxyTableDateAdded[key] + 7200000 < time) {
-      delete this.proxyTable[key];
-      delete this.proxyTableDateAdded[key];
+class ProxyTableFacade {
+  constructor(...allowedProxyArr) {
+    this.allowedProxy = new Set(...allowedProxyArr);
+    this.proxyTable = {};
+    this.proxyTableDateAdded = {};
+    var self = this;
+    setInterval(function() {
+      self.deleteOldProxyUrl();
+    }, 7200000);
+  }
+  add(url) {
+    let status;
+    if (this.allowedProxy.has(url)) {
+      status = true;
+    }
+    let matches = url.match(/(^http.*\/\/.*)\//);
+    if (matches && !status && this.allowedProxy.has(matches[1])) {
+      status = true;
+    }
+    if (!status) {
+      return "";
+    }
+    var proxyUrl = "/" + md5(url);
+    this.proxyTable[proxyUrl] = url;
+    this.proxyTableDateAdded[proxyUrl] = Date.now();
+    return host + proxyUrl;
+  }
+  deleteOldProxyUrl() {
+    var time = Date.now();
+    for (var key in this.proxyTableDateAdded) {
+      if (this.proxyTableDateAdded[key] + 7200000 < time) {
+        delete this.proxyTable[key];
+        delete this.proxyTableDateAdded[key];
+      }
     }
   }
-};
+}
 
-const proxyTableFacade = new ProxyTableFacade();
+const proxyTableFacade = new ProxyTableFacade([
+  "http://xn--42-mlcqimbe0a8d2b.xn--p1ai"
+]);
 
 app.get("/get-url-proxy", function(req, res) {
-  var status;
-  var urlProxy;
-  if (typeof req.query.url !== "undefined") {
-    status = true;
-    errorText = "";
-    urlProxy = proxyTableFacade.add(req.query.url);
-  } else {
-    status = false;
+  let status = true;
+  let urlProxy = "";
+  let errorText = "";
+  if (typeof req.query.url === "undefined") {
     errorText = "url undefined";
     urlProxy = "";
+    status = false;
   }
+
+  if (status) {
+    urlProxy = proxyTableFacade.add(req.query.url);
+  }
+
+  if (status && !urlProxy) {
+    status = false;
+    errorText = "not allowed";
+  }
+
   res.send({
     status,
     errorText,
@@ -53,17 +76,33 @@ app.get("/get-url-proxy", function(req, res) {
 });
 
 app.get("/proxy", function(req, res) {
-  var urlProxy;
-  if (typeof req.query.url !== "undefined") {
-    status = true;
-    errorText = "";
-    urlProxy = proxyTableFacade.add(req.query.url);
-  } else {
-    status = false;
+  let status = true;
+  let urlProxy = "";
+  let errorText = "";
+  if (typeof req.query.url === "undefined") {
     errorText = "url undefined";
     urlProxy = "";
+    status = false;
   }
-  res.redirect(urlProxy);
+
+  if (status) {
+    urlProxy = proxyTableFacade.add(req.query.url);
+  }
+
+  if (status && !urlProxy) {
+    status = false;
+    errorText = "not allowed";
+  }
+
+  if (status && urlProxy) {
+    res.redirect(urlProxy);
+  } else {
+    res.send({
+      status,
+      errorText,
+      urlProxy
+    });
+  }
 });
 
 const options = {
